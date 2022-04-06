d709ffa3de0c5fb96ffb715dc896373da722686a
Sohail3010 <58811697+Sohail3010@users.noreply.github.com>
2022-04-06 13:59:34 +0100

    Add files via upload

---
+++ oop/oop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            int x = 10;
+            int y = 5;
+            int sum = 0;
+            sum = calc.Add(x, y);
+            Console.WriteLine("sum of {0} + {1} = {2}", x, y, sum);
+            sum = calc.Subtract(x, y);
+            Console.WriteLine("sum of {0} - {1} = {2}", x, y, sum);
+            sum = calc.Divide(x, y);
+            Console.WriteLine("sum of {0} / {1} = {2}", x, y, sum);
+            Console.ReadLine();
+        }
+
+      
+    }   
+}
