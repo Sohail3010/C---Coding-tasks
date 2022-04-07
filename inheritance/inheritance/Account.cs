7f9456558b2827e248637ae18ad28b9ac597c061
Sohail3010 <58811697+Sohail3010@users.noreply.github.com>
2022-04-07 12:44:33 +0100

    Add files via upload

---
+++ inheritance/inheritance/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    class Account : Person
+    {
+        //Constructor
+        public Account() { }
+        public double getAccountBalance()
+        {
+            return currentAccount;
+        }
+        public double setAccountBalance(double amount)
+        {
+            currentAccount = currentAccount + amount;
+            return currentAccount;
+        }
+        
+        public double withdrawFromAccount(double amountToWithDraw)
+        {
+            currentAccount = currentAccount - amountToWithDraw;
+            return currentAccount;
+        }
+
+        internal double getAccountBalance(double amountToWithDraw)
+        {
+            currentAccount = currentAccount - amountToWithDraw;
+            return currentAccount;
+        }
+    }
+}
