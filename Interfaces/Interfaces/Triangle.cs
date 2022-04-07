f814247edc632837bbeb85c7026d11f3e18f544f
Sohail3010 <58811697+Sohail3010@users.noreply.github.com>
2022-04-07 12:44:00 +0100

    Add files via upload

---
+++ Interfaces/Interfaces/Triangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Triangle : TShape
+    {
+        public double height { get; set; }
+        public double width { get; set; }
+
+        public double Area()
+        {
+            return height * width;
+        }
+    }
+}
