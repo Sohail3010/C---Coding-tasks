6d9e68c580aff36bfe2134d12194f5ed14da8283
Sohail3010 <58811697+Sohail3010@users.noreply.github.com>
2022-04-08 15:27:24 +0100

    Add files via upload

---
+++ Collections/ArrayList/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList
+{
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int age { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Numbers = new List<int>();
+            Random r = new Random();
+            for (int x=0;x<10;x++)
+            {
+                foreach (var number in Numbers)
+                { Console.Write(number + " "); }
+                Console.WriteLine("__________________");
+                Console.WriteLine("Average of random numbers is: {0}",
+                    Numbers.Average());
+                Numbers.Insert(10, 0);
+                var index = Numbers.IndexOf(0);
+                Console.WriteLine("index of position 0 is :{0}", index);
+                var count = Numbers.Count();
+                Console.WriteLine("count is:{0},count");
+                var Student = new List<Person>
+                {
+                    new Person(){FirstName="Joe",LastName="Bloggs",age=23},
+                    new Person(){FirstName="Steve",LastName="May",age=22},
+                    new Person(){FirstName="Jack",LastName="Jones",age=24},
+                    new Person(){FirstName="Tom",LastName="Jones",age=23},
+                };
+                Console.WriteLine("numbers of object is:{0}", Student.Count());
+                foreach (var student in Student)
+                {
+                    Console.WriteLine(student.FirstName);
+                    Console.WriteLine(student.LastName);
+                    Console.WriteLine(student.age);
+                    Console.WriteLine("______________________");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a list of strings
+            var Names = new List<string>();
+            Names.Add("Joe");
+            Names.Add("Bob");
+            Names.Add("Jack");
+            Names.Add("Steve");
+            //iterate through the list
+            foreach (var name in Names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine("____________________________________");
+            var Numbers = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            foreach(var num in Numbers)
+            {
+                if(num%2 == 0)
+                {
+                    // print out even numbers
+                    Console.WriteLine(num);
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
